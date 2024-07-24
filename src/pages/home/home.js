f48b19d202b9c6b88e3a6c01628f5d2a554d41ad
icaro <icaroarmando21@gmail.com>
2024-07-24 12:12:53 -0300

    commit

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -5,3 +5,37 @@ function logout() {
         alert('Erro ao fazer logout');
     })
 }
+
+const fakeTransactions = [{
+    type: 'expense',
+    date: '2022-01-04',
+    money: {
+        currency: 'R$',
+        value: 10
+    },
+    transactionTape: 'supermercado'
+}, {
+    type: 'expense',
+    date: '2022-01-09',
+    money: {
+        currency: 'R$',
+        value: 1000
+    },
+    transactionTape: 'salário'
+}, {
+    type: 'expense',
+    date: '2022-01-05',
+    money: {
+        currency: 'R$',
+        value: 109
+    },
+    transactionTape:'supermercado'
+}, {
+    type: 'expense',
+    date: '2022-01-06',
+    money: {
+        currency: 'R$',
+        value: 108
+    },
+    transactionTape:'supermercado'
+}]
